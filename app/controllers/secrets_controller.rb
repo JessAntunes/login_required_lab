62da09f2ed8d77740eff1013368be4ab84798165
Jessica Antunes <jessantunes@protonmail.com>
2021-05-04 19:54:55 -0400

    commit tree 4f8d627aaa2a1c7bc511e9c857e2a83519f89873

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+    
+
+    def show
+        if !logged_in?
+            redirect_to login_path
+        else
+            
+        end
+    end
+
+    def create
+        
+    end
+
+end
